28963d635f596d5efed3b2cc5fd51c3d06f8aa76
coder-utshab <ghoshutshob470@gmail.com>
2026-01-30 23:14:51 +0530

    js

---
+++ if-else practice.js
@@ -0,0 +1,10 @@
+const number = 23;
+//string, number, boolean
+if(typeof number === 'number'){
+    console.log('value is a number')
+}
+else{
+    console.log('value is  not a number')
+}
+const numbers = [3, 12, 78, 90];
+console.log(typeof numbers);
