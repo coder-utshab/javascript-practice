34ea3de9a5f6c5b4f081bbbd38dfbb8d972fa63c
coder-utshab <ghoshutshob470@gmail.com>
2026-01-30 23:07:12 +0530

    up

---
+++ jpractice-element.js
@@ -0,0 +1,26 @@
+/*
+amra kevaba  html a ar moddo javascript ar kag ke vaba korbo
+
+1. how to use js with html and css
+2.get element or elements from dom
+2-1: getElementById
+2-2: getElementsByClassName
+2-3: querySelector
+2-4: querySelectorAll
+
+3.  dynamically and element to the dom: document.ccreateElement
+    3-5: set value to the innnerHTML
+
+4.   get and  value from/to a dom element
+     4-1: use innerText (h1, p, 5, h5, div, span, strong)
+     4-2: use value (inpur,textArea)
+
+5.   add event listner (event handler)
+     5-1. click, focus, blur, onchange, keyup
+     5-2: addEventListner onclick="myHandler()"
+
+6.  use function reduce duplicate code
+
+
+7.  event double and event dedigate
+*/
