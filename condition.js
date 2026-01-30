437b3ded280e87e16cfda2003df302090294d468
coder-utshab <ghoshutshob470@gmail.com>
2026-01-31 00:04:16 +0530

    up

---
+++ condition.js
@@ -0,0 +1,18 @@
+const number = 23;
+//string, number, boolean
+if(typeof number === 'number'){
+    console.log('value is a number')
+}        
+else{
+    console.log('value is a not a number')
+}
+
+
+//ARRAY DECLEAR KORAR NEOME
+const numbers = [3, 12, 78, 90];
+const student = {name: 'Utshab Kumar Ghosh', job: 'Freelanching'} 
+
+// console.log(Array.isArray(numbers));
+console.log(Array.isArray(number))
+console.log(typeof student);
+
