613143ea091c72f78c4c0f0dd0c1403929223a7b
coder-utshab <ghoshutshob470@gmail.com>
2026-01-31 15:36:02 +0530

    up

--- var-let-const.js
+++ var-let-const.js
@@ -1,3 +1,33 @@
 //var: no reason to use  var
 //let: allow it to ressign
 //const: do not allow it to rassign
+
+
+let count = 0;
+count = count + 10;
+console.log(count)
+
+
+
+//-------------------------------------------
+const numbers = [23, 4, 32, 12, 54];
+numbers [1] = 55;
+numbers.push(8, 9, 65, 32);
+console.log(numbers);
+
+
+//object decclear------------------------
+const student = {
+    name: 'Utshab Kumar Ghosh',
+    class: 16
+}
+console.log(student);
+
+
+//variable declear korbo let deya
+
+let product = {
+    name: 'rice',
+    class: 1600
+}
+console.log(product)
