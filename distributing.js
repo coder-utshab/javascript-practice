332be9bd2524b3de058692522c5f6874e86f2559
coder-utshab <ghoshutshob470@gmail.com>
2026-01-31 18:13:49 +0530

    up

---
+++ distributing.js
@@ -0,0 +1,17 @@
+
+//amra jodii kono object ak ar bashy bar neta chay tahola avba process korbo
+
+const sajib = {
+    name: 'Utshab',
+    age: 25,
+    phone: '01771740780',
+    money: 134456566987
+}
+
+const {phone, age} = sajib
+console.log(phone)
+console.log(phone)
+console.log(phone)
+console.log(age)
+console.log(age)
+console.log(age)
