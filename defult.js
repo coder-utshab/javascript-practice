81a38559b6bbfa75d3beb652e62cb6cdf58fc115
coder-utshab <ghoshutshob470@gmail.com>
2026-01-31 16:01:39 +0530

    sum

--- defult.js
+++ defult.js
@@ -6,3 +6,14 @@ function add(num1, num2){
 }
 const sum = add(5, 7);
 //output-- 12
+
+
+
+//defult ---> if value is not provided take this as a defult.
+
+function add(num1 = 99, num2 = 0, num3 = 12){
+    const result = num1 + num2 + num3;
+    console.log(num1, num2, result);
+    return result;
+}
+const sum = add(5);
