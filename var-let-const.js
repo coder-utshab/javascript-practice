f74562f97688a2114aab0c82e99c2502132eda76
coder-utshab <ghoshutshob470@gmail.com>
2026-01-31 15:42:50 +0530

    let

--- var-let-const.js
+++ var-let-const.js
@@ -3,6 +3,11 @@
 //const: do not allow it to rassign
 
 
+//VIP TOPICKS
+//variable declear korbo sobsomoy const deya
+//variable ar man ta amra jodii set korta chay tayla let deya korbo
+
+
 let count = 0;
 count = count + 10;
 console.log(count)
@@ -31,3 +36,13 @@ let product = {
     class: 1600
 }
 console.log(product)
+
+
+
+//loop chalanor jonno----------------------
+
+let sum = 0;
+for(let i = 0; i <10; i++){
+    sum = sum + i;
+}
+console.log(sum);
