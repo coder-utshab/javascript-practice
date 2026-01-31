a500718632af77ab3659435652c031cc67ead109
coder-utshab <ghoshutshob470@gmail.com>
2026-01-31 16:53:44 +0530

    ar

---
+++ arrow.js
@@ -0,0 +1,24 @@
+
+//function declearation
+
+// function add (a, b){
+//     const result = a + b;
+//     return result;
+// }
+// const sum = add(5, 90);
+// console.log(sum);
+
+
+
+//function expression
+
+// const add2 = function(a, b){
+//     return a + b;
+// }
+// const sum = add(5, 90);
+// console.log(sum);
+
+//arrow function
+const add3 = (a, b) => a + b
+const sum = add3(5, 94);
+console.log(sum);
--- templeate.js
+++ templeate.js
@@ -3,3 +3,12 @@ const last = 'utshab'
 // const name = first + last;
 const name = first +' '+ last;
 console.log(name);
+
+
+// dynamic string
+const a = 10;
+const b = 20;
+const messeage = `The Sun Of ${a} and ${b} is ${a+b}`;
+console.log(messeage);
+//output
+//The Sun Of 10 and 20 is 30
