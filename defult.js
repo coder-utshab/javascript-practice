30db039fd2a884e7d30f3351e05598892aff9eb4
coder-utshab <ghoshutshob470@gmail.com>
2026-01-31 15:48:12 +0530

    cp

---
+++ defult.js
@@ -0,0 +1,8 @@
+//function declear korar jonno 
+function add(num1, num2){
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    return result;
+}
+const sum = add(5, 7);
+//output-- 12
